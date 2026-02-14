b02eafa59fbce9ec85cd7164c03ed7ebe8fb070a
Cry Tokyo <114428720+Cry-Tokyo@users.noreply.github.com>
2026-02-14 16:03:04 -0500

    Refactor install.sh to use bash and add menu
    Updated script to use bash and added server selection loop.

--- install.sh
+++ install.sh
@@ -1,2 +1,33 @@
-#!/bin/sh
-sudo apt install git
+#!/bin/bash
+clear;
+#sudo apt install git;
+#sudo systemctl enable nftables.service;
+#sudo systemctl start nftables.service;
+while true
+do
+    echo -e 'Which Server to Install?\n 0. Frontend\n 1. DMZ\n 2. Middleware\n 3. Database';
+    read server_selection;
+    case "$server_selection" in
+      0)
+        echo "love";
+        clear
+      ;;
+      1)
+        echo "love";
+        clear
+      ;;
+      2)
+        echo "love";
+        clear
+      ;;
+      3)
+        echo "love";
+        clear
+      ;;
+      *)
+        clear;
+        echo "Choose between 0-3.";
+      ;;
+      esac
+
+done
