024fd214c884c01da5f68e92b84c4ee3d847dc6c
Jessii242 <jf489@njit.edu>
2026-03-07 03:04:05 +0000

    everything should be good now

--- DMZ/fetchDataCron.php
+++ DMZ/fetchDataCron.php
@@ -1,9 +1,9 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__ . '/fetchData.php';
+require_once __DIR__ . '/config.php';
 //fixed window, 1 hr
-$daysBack =DEFAULT_DAYS_BACK;
-$data=fetchBooks($daysBack);
+$data=fetchBooks($searchTerms);
 echo "[" . date('c'). "] Starting cron job. \n";
 if($data==null){
 	echo"[" . date('c'). "] Cron failed to fetch data :c \n";
