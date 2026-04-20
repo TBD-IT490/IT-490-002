feb6176d7d5742002e766b9ca2a2f6b117ed81b5
Cry-Tokyo <mc2279@njit.edu>
2026-04-20 17:34:41 +0000

    looks like final changes

--- Dmz/apiListener.php
+++ Dmz/apiListener.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-require_once __DIR__ . '/config.php';
+//require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/fetchData.php';
 require_once __DIR__ .'/vendor/autoload.php'; /** rmq library */
 use PhpAmqpLib\Connection\AMQPStreamConnection; /**import RMQ classes*/
@@ -11,13 +11,25 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Monolog\Formatter\LineFormatter;
 
-//require_once __DIR__ . '/vendor/autoload.php';
-//require_once __DIR__ realpath(__DIR__ . '/vendor/autoload.php');
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $backend = $_ENV['BACKEND']; //for rabbit and db
 
+
+
+
+
+
+define ('API_BASE', 'https://www.googleapis.com/books/v1/volumes');
+define ('API_KEY', 'AIzaSyDYJbl3JpctqD5r3bn_qF4LkCzBvjOfdQI');
+
+//for rabbit
+define ('RABBITMQ_HOST',$backend); // change to matt's ip 100.127.138.110
+define ('RABBITMQ_PORT', 5672);
+define ('RABBITMQ_USER', 'broker'); //change to broker rabbit user
+define ('RABBITMQ_PASS', 'test'); // change to test rabbit pass
+define ('RABBITMQ_QUEUE', 'api_queue'); //change queue name to actual name
 
 
 class RabbitMQLOG extends AbstractProcessingHandler {
@@ -122,6 +134,38 @@ function refreshCache() {
     }
     echo "HORRAY! Cache refresh complete.\n";
 }
+function buildURL(string $searchTerm): string
+{
+	
+	$query = http_build_query(['q'=>$searchTerm, 'maxResults'=>10, 'key'=>API_KEY]);
+	return API_BASE . '?' . $query;
+}
+function cleanData($data){
+	$book = [];
+	for($i = 0; $i < count($data["items"] ?? []); $i++) {
+	/// print array
+    $book[$i]['api_book_id'] = $data["items"][$i]['id'] ?? null;
+    $book[$i]['isbn'] = $data["items"][$i]['volumeInfo']['industryIdentifiers'][1]['identifier'] ?? null;
+    $book[$i]['title'] = $data["items"][$i]['volumeInfo']['title'] ?? null;
+    $book[$i]['subtitle'] = $data["items"][$i]['volumeInfo']['subtitle'] ?? null;
+    $book[$i]['author'] = $data["items"][$i]['volumeInfo']['authors'][0] ?? null;
+    $book[$i]['description'] = $data["items"][$i]['volumeInfo']['description'] ?? null;
+    $book[$i]['cover_url'] = $data["items"][$i]['volumeInfo']['imageLinks']['thumbnail'] ?? null;
+    $book[$i]['publisher'] = $data["items"][$i]['volumeInfo']['publisher'] ?? null;
+    $published_date = $data["items"][$i]['volumeInfo']['publishedDate'] ?? null;
+	if (strlen($published_date) != 4){ 
+		$published_date = date("Y", strtotime($published_date));
+	}
+	$book[$i]['published_year'] = $published_date;
+	
+	$book[$i]['genre'] = $data["items"][$i]['volumeInfo']['categories'][0] ?? null;
+    $book[$i]['maturity_rating'] = $data["items"][$i]['volumeInfo']['maturityRating'] ?? null;
+    $book[$i]['content_version'] = $data["items"][$i]['volumeInfo']['contentVersion'] ?? null;
+	$book[$i]['pages'] = $data["items"][$i]['volumeInfo']['pageCount'] ?? 0 ;
+	}
+	echo '' . print_r($book, true) .'';
+    return $book;
+}
 //RMQ processing
 //ctrl c ctrl v from db listener
 //chat am i doing this right??
@@ -155,6 +199,56 @@ try {
 }
 
     return processPublishBooks($clean_data);
+}
+function processPublishBooks(array $data){
+	//$seenFile= __DIR__ . '/bookIDsInQueue.txt';
+	//$seen = file_exists($seenFile) ? array_flip(file($seenFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)) : [];
+	$count=count($data);
+	$books = [];
+	/*foreach($data as $book){
+
+		//turns book entry into json string
+		$json=json_encode($book);
+
+
+		publishToRabbit($json);
+		$count++;
+		if($count > 10){ 
+			break;
+		}
+	}*/
+	$json=json_encode($data);
+	return $json;
+	//publishToRabbit($json);
+	echo "Published $count books to RabbitMQ :D !!\n";
+	//if($count > 0){return true;} else {return false;}
+}
+function fetchBooks(string $searchTerm): ?array{
+	$url=buildURL($searchTerm);
+	echo''.$url.'';
+	$ch=curl_init($url);
+	curl_setopt_array($ch, [CURLOPT_RETURNTRANSFER => true, CURLOPT_TIMEOUT=>10]);
+	$response=curl_exec($ch);
+	$httpCode=curl_getinfo($ch,CURLINFO_HTTP_CODE);	
+
+	//trying to fix that 429 error :c
+	if($httpCode==429){
+		echo"Google hates us, waiting 5 seconds...\n";
+		sleep(5);
+		return null;//fetchBooks($searchTerm);
+	}
+	//deals with other errors	
+	if($response==false || $httpCode!==200){
+		fwrite(STDERR, "Error fetching book api :( \nHTTP code: $httpCode\n");
+		curl_close($ch);
+		return null;
+	}
+	curl_close($ch);
+	$data=json_decode($response,true);
+	if($data==null){
+		fwrite(STDERR, "Error decoding JSON from book api :| \n");
+		return null;
+	} return $data;
 }
 function onDemandAPICall($data) {
     $searchTerm = $data['search_query'] ?? '';
