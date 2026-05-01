5155989bd43a4ac0c6e75ad1e64ed6d971c09778
Cry Tokyo <114428720+Cry-Tokyo@users.noreply.github.com>
2026-05-01 16:59:18 -0400

    Comment out fetchData.php inclusion
    Comment out the fetchData.php require statement.

--- Dmz/apiListener.php
+++ Dmz/apiListener.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 //require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/fetchData.php';
+//require_once __DIR__ . '/fetchData.php';
 require_once __DIR__ .'/vendor/autoload.php'; /** rmq library */
 use PhpAmqpLib\Connection\AMQPStreamConnection; /**import RMQ classes*/
 use PhpAmqpLib\Message\AMQPMessage;
