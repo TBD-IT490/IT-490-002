dca472238a3aeb5ea3ab45aa738378da17ea2757
Taryn <faccendataryn@gmail.com>
2026-05-08 23:34:19 +0000

    making sure the shopping cart actually works (it almost does)

--- Frontend/shop_files/cart.php
+++ Frontend/shop_files/cart.php
@@ -1,100 +1,195 @@
 <?php
+//index.php
 session_start();
 require_once '../includes/header.php';
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP Shopping Cart with Stripe Payment Integration</title>
+		<link rel="stylesheet" href="../styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<style>
+		.popover
+		{
+		    width: 100%;
+		    max-width: 800px;
+		}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<br />
+			<h3 align="center"><a href="#">PHP Shopping Cart with Stripe Payment Integration</a></h3>
+			<br />
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Menu</span>
+						<span class="glyphicon glyphicon-menu-hamburger"></span>
+						</button>
+						<a class="navbar-brand" href="/">Webslesson</a>
+					</div>
+					<div id="navbar-cart" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li>
+								<a id="cart-popover" class="btn" data-placement="bottom" title="Shopping Cart">
+									<span class="glyphicon glyphicon-shopping-cart"></span>
+									<span class="badge"></span>
+									<span class="total_price">$ 0.00</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			
+			<div id="popover_content_wrapper" style="display: none">
+				<span id="cart_details"></span>
+				<div align="right">
+					<a href="order_process.php" class="btn btn-primary" id="check_out_cart">
+						<span class="glyphicon glyphicon-shopping-cart"></span> Check out
+					</a>
+					<a href="#" class="btn btn-default" id="clear_cart">
+						<span class="glyphicon glyphicon-trash"></span> Clear
+					</a>
+				</div>
+			</div>
 
-<doctype html>
-<h1 class="my-4">Noetic's Blind Date with a Book</h1>
-<div class="container">
+			
+			
+			<?php
+				if(isset($_SESSION["success_message"]))
+				{
+					echo '
+					<div class="alert alert-success">
+					'.$_SESSION["success_message"].'
+					</div>
+					';
+					unset($_SESSION["success_message"]);
+				}
+				?>
 
-<!--book 1-->
-  <div class="row g-4">
-    <div class="col-sm-6 col-md-4 col-lg-3">
-      <div class="n-card p-3 h-100 d-flex flex-column">
-        <h5 class="n-card-title">Mystery Novel</h5>
-        <img src="mystery_book.jpg" alt="Mystery Novel" class="n-card-img mb-3">
-        <p class="n-card-text">
-          A thrilling mystery novel that will keep you on the edge of your seat.
-        </p>
-        <div class="mt-auto">
-          <p><strong>$19.99</strong></p>
-          <form method="post" action="add_to_cart.php">
-            <button type="submit" class="btn btn-primary">Add To Cart</button>
-          </form>
-        </div>
-      </div>
-    </div>
+			<div id="display_item" class="row">
 
-    <!--book 2-->
-    <div class="col-sm-6 col-md-4 col-lg-3">
-      <div class="n-card p-3 h-100 d-flex flex-column">
-        <h5 class="n-card-title">Romance Novel</h5>
-        <img src="mystery_book.jpg" alt="Mystery Novel" class="n-card-img mb-3">
-        <p class="n-card-text">
-          An exciting love story for the ages, it will leave you wanting more!
-        </p>
-        <div class="mt-auto">
-          <p><strong>$19.99</strong></p>
-          <form method="post" action="add_to_cart.php">
-            <button type="submit" class="btn btn-primary">Add To Cart</button>
-          </form>
-        </div>
-      </div>
-    </div>
+			</div>
 
-    <!--book 3-->
-    <div class="col-sm-6 col-md-4 col-lg-3">
-      <div class="n-card p-3 h-100 d-flex flex-column">
-        <h5 class="n-card-title">Action Novel</h5>
-        <img src="mystery_book.jpg" alt="Mystery Novel" class="n-card-img mb-3">
-        <p class="n-card-text">
-          A breathtaking action novel that will leave you jumping out of your seat!
-        </p>
-        <div class="mt-auto">
-          <p><strong>$19.99</strong></p>
-          <form method="post" action="add_to_cart.php">
-            <button type="submit" class="btn btn-primary">Add To Cart</button>
-          </form>
-        </div>
-      </div>
-    </div>
-
-    <!--book 4-->
-    <div class="col-sm-6 col-md-4 col-lg-3">
-      <div class="n-card p-3 h-100 d-flex flex-column">
-        <h5 class="n-card-title">Comedy Novel</h5>
-        <img src="mystery_book.jpg" alt="Mystery Novel" class="n-card-img mb-3">
-        <p class="n-card-text">
-            A knee slapping comedy that will make you laugh till you cry!
-        </p>
-        <div class="mt-auto">
-          <p><strong>$19.99</strong></p>
-          <form method="post" action="add_to_cart.php">
-            <button type="submit" class="btn btn-primary">Add To Cart</button>
-          </form>
-        </div>
-      </div>
-    </div>
-
-    <!--book 5-->
-    <div class="col-sm-6 col-md-4 col-lg-3">
-      <div class="n-card p-3 h-100 d-flex flex-column">
-        <h5 class="n-card-title">Sci-Fi Novel</h5>
-        <img src="mystery_book.jpg" alt="Mystery Novel" class="n-card-img mb-3">
-        <p class="n-card-text">
-          Aliens?? Monsters?? Find out more in this epic sci-fi adventure!
-        </p>
-        <div class="mt-auto">
-          <p><strong>$19.99</strong></p>
-          <form method="post" action="add_to_cart.php">
-            <button type="submit" class="btn btn-primary">Add To Cart</button>
-          </form>
-        </div>
-      </div>
-    </div>
-
-  </div>
-</div>
+				
+				<br />
+				<br />
+			</div>
+		</div>
+	</body>
 </html>
 
-<?php require_once '../includes/footer.php'; ?>
+<script>  
+$(document).ready(function(){
+
+	load_product();
+
+	load_cart_data();
+
+	function load_product()
+	{
+		$.ajax({
+			url:"fetch_item.php",
+			method:"POST",
+			success:function(data)
+			{
+				$('#display_item').html(data);
+			}
+		})
+	}
+
+	function load_cart_data()
+	{
+		$.ajax({
+			url:"fetch_cart.php",
+			method:"POST",
+			dataType:"json",
+			success:function(data)
+			{
+				$('#cart_details').html(data.cart_details);
+				$('.total_price').text(data.total_price);
+				$('.badge').text(data.total_item);
+			}
+		})
+	}
+
+	$('#cart-popover').popover({
+		html : true,
+		container : 'body',
+		content:function(){
+			return $('#popover_content_wrapper').html();
+		}
+	});
+
+	$(document).on('click', '.add_to_cart', function(){
+		var product_id = $(this).attr('id');
+		var product_name = $('#name'+product_id+'').val();
+		var product_price = $('#price'+product_id+'').val();
+		var product_quantity = $('#quantity'+product_id).val();
+		var action = 'add';
+		if(product_quantity > 0)
+		{
+			$.ajax({
+				url:"action.php",
+				method:"POST",
+				data:{product_id:product_id, product_name:product_name, product_price:product_price, product_quantity:product_quantity, action:action},
+				success:function(data)
+				{
+					load_cart_data();
+					alert("Item has been Added into Cart");
+				}
+			})
+		}
+		else
+		{
+			alert("Please Enter Number of Quantity");
+		}
+	});
+
+	$(document).on('click', '.delete', function(){
+		var product_id = $(this).attr('id');
+		var action = 'remove';
+		if(confirm("Are you sure you want to remove this product?"))
+		{
+			$.ajax({
+				url:"action.php",
+				method:"POST",
+				data:{product_id:product_id, action:action},
+				success:function(data)
+				{
+					load_cart_data();
+					$('#cart-popover').popover('hide');
+					alert("Item has been removed from Cart");
+				}
+			})
+		}
+		else
+		{
+			return false;
+		}
+	});
+
+	$(document).on('click', '#clear_cart', function(){
+		var action = 'empty';
+		$.ajax({
+			url:"action.php",
+			method:"POST",
+			data:{action:action},
+			success:function()
+			{
+				load_cart_data();
+				$('#cart-popover').popover('hide');
+				alert("Your Cart has been clear");
+			}
+		})
+	});
+    
+});
+
+</script>
