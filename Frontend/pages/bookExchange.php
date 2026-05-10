fe63a285659699277243a219d815f737c1b18f9b
Taryn <faccendataryn@gmail.com>
2026-05-10 04:08:09 +0000

    chugging along with exchange design

--- Frontend/pages/bookExchange.php
+++ Frontend/pages/bookExchange.php
@@ -8,6 +8,7 @@ require_once '../includes/data.php';
 <html lang="en">
     <head>
     <h1 class="my-4">Trade and Buy Books with Noetic</h1>
+    </head> 
     <div class="container">
         <div class ="row g-4">
             <div class="col-sm-6 col-md-4 col-lg-3">
@@ -17,8 +18,59 @@ require_once '../includes/data.php';
                 </div>
             </div>
         </div>
-    </div>
-</head>  
+        <br></br>
+    <h2>Browse Books</h2>
+        <div class="book-ex-grid">
+            <?php foreach ($availableBooks as $book): ?>
+                <div class="book-ex-card">
+                    <div class="book-ex-content">
+                        <div class="condition">
+                            <? = $book['condition']; ?>
+                        </div>
+
+                        <h3><?= $book['title']; ?></h3>
+                        <p><?= $book['author']; ?></p>
+
+                        <div class="owner">
+                            Owner: <? $book['ownerName']; ?><br>
+                            <?= $book['ownerLocation']; ?>
+                        </div>
+
+                        <button class="ex-btn">
+                            Request Trade
+                        </button>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div> 
+
+    <!--MY OWNED BOOKS or my books up for trade i forgot-->
+    <h2>My Books</h2>
+
+        <div class="book-ex-grid">
+            <?php foreach ($myBooks as $book): ?>
+                <div class="book-ex-card">
+                    <div class="book-ex-content">
+                        <div class="condition">
+                            <?= $book['condition']; ?>
+                        </div>
+                    
+                        <h3><?= $book['title']; ?></h3>
+                        <p><?= $book['author']; ?></p>
+
+                        <div class="owner">
+                            Owner: <? $book['ownerName']; ?><br>
+                            <?= $book['ownerLocation']; ?>
+                        </div>
+
+                        <button class="ex-btn"> Remove Listing</button>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            </div>
+        </div>
+
 </html>
 
 
